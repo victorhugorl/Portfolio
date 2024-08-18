b8411d8fcb58e19f30d3962f2574be8e26019259
victor <victorlimasg@email.com>
2024-08-18 17:08:02 -0300

    making projects modal

---
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, signal, OnInit } from "@angular/core";
+
+//Interface
+import { IProjects } from "../../../interface/IProjects.inteface";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from "@angular/material/dialog";
+
+@Component({
+  selector: "app-dialog-projects",
+  standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: "./dialog-projects.component.html",
+  styleUrl: "./dialog-projects.component.scss",
+})
+export class DialogProjectsComponent implements OnInit {
+  constructor(
+    private _dialogRef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: IProjects
+  ) {}
+
+  public getData = signal<IProjects | null>(null);
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+
+  public closeModal() {
+    return this._dialogRef.close();
+  }
+}
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,27 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 
-//Interface
+// Interface
 import { IProjects } from "../../interface/IProjects.inteface";
+
+// Material
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+
+// Enum
+import { EDialogPanelClass } from "../../enum/EDialogPanelClass.enum";
+
+// Dialog
+import { DialogProjectsComponent } from "../dialog/dialog-projects/dialog-projects.component";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: "./projects.component.html",
   styleUrl: "./projects.component.scss",
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: "assets/img/projects/vfull.png",
@@ -18,13 +29,21 @@ export class ProjectsComponent {
       title: "Vida FullStack",
       width: "100px",
       height: "51px",
-      description: "",
+      description:
+        '<p class="modal">Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p class="modal">Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
       links: [
         {
-          name: "Conheça o blog",
+          name: "Conheça o Blog",
           href: "https://vidafullstack.com.br",
         },
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
